1f3a425afba3a5bf066651117f93a5ca3136f63a
lelwrence <47617960+lelwrence@users.noreply.github.com>
2022-03-04 12:25:13 -0500

    Add files via upload

--- courriel.js
+++ courriel.js
@@ -4,7 +4,7 @@ class Contact {
         this.shortName = shortName;
         this.longName = longName;
         //this.key = "";
-        this.lastUpdate = Date(Date.now());
+        this.lastUpdate = new Date();
     }
 }
 
@@ -13,17 +13,22 @@ class Message {
         this.contactId = contactId;
         this.subject = subject;
         this.message = message;
-        this.lastUpdate = Date(Date.now());
+        this.lastUpdate = new Date();
     }
 }
 
 var arrayContacts = [];
+//refreshContactsTable();
+refreshMsgContacts();
+
+/* //fix this
 if (localStorage.getItem("arrayContacts") === null) {
     localStorage.setItem("arrayContacts", arrayContacts);
 } else {
     var arrayContacts2 = localStorage.getItem("arrayContacts");
     arrayContacts2 = Object.values(arrayContacts2);
 }
+*/
 
 function isExistingContact(newContact) {
     for (var i = 0; i < arrayContacts.length; i++) {
@@ -35,7 +40,7 @@ function isExistingContact(newContact) {
 }
 
 function saveContact() {
-    let frmShortName = document.forms["frmContacts"]["contactShortName"].value;
+    let frmShortName = document.getElementById("contactShortName").value; //document.forms["frmContacts"]["contactShortName"].value;
     let frmLongName = document.getElementById("contactLongName").value; //document.forms["frmContacts"]["contactLongName"].value;
 
     //alert(document.getElementById("contactShortName").value);
@@ -54,33 +59,77 @@ function saveContact() {
     localStorage.setItem("arrayContacts", JSON.stringify(arrayContacts));
 
     refreshContactsTable();
+    refreshMsgContacts();
 }
 
-function addContactTableRow(ID, shortName, longName, lastUpdate) {
-    var tbodyRef = document.getElementById('tblContacts').getElementsByTagName('tbody')[0];
+function editContact() {
+
+}
+
+function deleteContact() {
+    
+}
+
+function addContactTableRow() {
+    var tbodyRef = document.getElementById("tblContacts").getElementsByTagName("tbody")[0];
+    var newRow = tbodyRef.insertRow();
+    var cellID = newRow.insertCell(0);
+    var cellShortName = newRow.insertCell(1);
+    var cellLongName = newRow.insertCell(2);
+    var cellLastUpdate = newRow.insertCell(3);
+    cellLongName.innerHTML = "Aucun record";
+}
+
+function addContactTableRow(ID, newContact) {
+    var tbodyRef = document.getElementById("tblContacts").getElementsByTagName("tbody")[0];
     var newRow = tbodyRef.insertRow();
     var cellID = newRow.insertCell(0);
     var cellShortName = newRow.insertCell(1);
     var cellLongName = newRow.insertCell(2);
     var cellLastUpdate = newRow.insertCell(3);
     cellID.innerHTML = ID;
-    cellShortName.innerHTML = shortName;
-    cellLongName.innerHTML = longName;
+    cellShortName.innerHTML = newContact.shortName;
+    cellLongName.innerHTML = newContact.longName;
 
-    lastUpdate = Date.parse(lastUpdate);
-    //const offset = lastUpdate.getTimezoneOffset();
-    //lastUpdate = new Date(lastUpdate.getTime() - (offset*60*1000));
+    //lastUpdate = Date.parse(lastUpdate);
+    var offset = newContact.lastUpdate.getTimezoneOffset();
+    var lastUpdate = new Date(newContact.lastUpdate.getTime() - (offset*60*1000));
 
-    cellLastUpdate.innerHTML = lastUpdate //.toISOString().split('T')[0];
+    cellLastUpdate.innerHTML = lastUpdate.toISOString(); //.split('T')[0];
 }
 
 function refreshContactsTable() {
-    document.getElementById('tblContactsBody').innerHTML = "";
+    var contactsTable = document.getElementById("tblContactsBody");
+    contactsTable.innerHTML = "";
+
     if (arrayContacts.length === 0) {
-        addContactTableRow("", "Aucun record", "", "");
+        addContactTableRow();
     } else {
         for (var i = 0; i < arrayContacts.length; i++) {
-            addContactTableRow(i, arrayContacts[i].shortName, arrayContacts[i].longName, arrayContacts[i].lastUpdate);
+            addContactTableRow(i, arrayContacts[i]);
         }
     }
+}
+
+function refreshMsgContacts() {
+    var msgContact = document.getElementById("msgContact");
+    msgContact.innerHTML = "";
+    console.log(arrayContacts.length);
+    for (var i = -1; i < arrayContacts.length; i++) {
+        var opt = document.createElement("option");
+        if (i == -1) {
+            if (arrayContacts.length === 0) {
+                opt.textValue = "Aucun contact";
+            } else {
+                opt.textValue = "Choisir contact";
+            }
+            opt.value = "0";
+        } else {
+            opt.textValue = arrayContacts[i].longName;
+            opt.value = arrayContacts[i].shortName;
+        }
+        msgContact.appendChild(opt);
+        console.log(opt);
+    }
+
 }
